de06de39c8f3a40fc9999e4cc632a05298e5a358
chanin <pci2676@gmail.com>
2020-11-07 18:47:58 +0900

    security project init

---
+++ bomsecurity/src/test/java/com/javabom/bomsecurity/BomsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javabom.bomsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BomsecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
